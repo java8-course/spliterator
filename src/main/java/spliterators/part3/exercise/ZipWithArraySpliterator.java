42daac4aee3ea843f7f99b230a01be110783b21b
Aleksandr_Petukhov <aleksandr_petukhov@epam.com>
2017-04-13 17:27:40 +0300

    ZipWithArraySpliterator wip

--- src/main/java/spliterators/part3/exercise/ZipWithArraySpliterator.java
+++ src/main/java/spliterators/part3/exercise/ZipWithArraySpliterator.java
@@ -1,5 +1,7 @@
 package spliterators.part3.exercise;
 
+import spliterators.part2.exercise.IndexedDoublePair;
+
 import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.function.Consumer;
@@ -9,40 +11,55 @@ public class ZipWithArraySpliterator<A, B> extends Spliterators.AbstractSplitera
 
     private final Spliterator<A> inner;
     private final B[] array;
+    private final int endIndex;
+    private int currentIndex;
 
     public ZipWithArraySpliterator(Spliterator<A> inner, B[] array) {
-        super(Long.MAX_VALUE, 0); // FIXME:
-        // TODO
-        throw new UnsupportedOperationException();
+        this(inner, array, 0, array.length);
+    }
+
+    private ZipWithArraySpliterator(Spliterator<A> inner, B[] array, int startIndex, int endIndex) {
+        super(array.length - startIndex, inner.characteristics());
+        this.inner = inner;
+        this.array = array;
+        currentIndex = startIndex;
+        this.endIndex = endIndex;
     }
 
     @Override
     public int characteristics() {
-        // TODO
-        throw new UnsupportedOperationException();
+        return inner.characteristics();
     }
 
     @Override
     public boolean tryAdvance(Consumer<? super Pair<A, B>> action) {
-        // TODO
-        throw new UnsupportedOperationException();
+        return currentIndex < endIndex && inner.tryAdvance(a -> action.accept(new Pair<A, B>(a, array[currentIndex++])));
     }
 
     @Override
     public void forEachRemaining(Consumer<? super Pair<A, B>> action) {
-        // TODO
-        throw new UnsupportedOperationException();
+        for (B b : array) {
+            currentIndex++;
+            if (!inner.tryAdvance(a -> action.accept(new Pair<A, B>(a, b)))) return;
+        }
     }
 
     @Override
     public Spliterator<Pair<A, B>> trySplit() {
-        // TODO
-        throw new UnsupportedOperationException();
+        if (inner.hasCharacteristics(SUBSIZED)) {
+            Spliterator<A> splittedInner = inner.trySplit();
+            currentIndex += splittedInner.estimateSize();
+
+            return new ZipWithArraySpliterator<>(splittedInner, array,
+                    (int)(currentIndex - splittedInner.estimateSize()), currentIndex);
+        }
+
+        return super.trySplit();
     }
 
     @Override
     public long estimateSize() {
-        // TODO
-        throw new UnsupportedOperationException();
+        if (inner.hasCharacteristics(SIZED)) return Math.min(inner.estimateSize(), endIndex - currentIndex);
+        return endIndex - currentIndex;
     }
 }
