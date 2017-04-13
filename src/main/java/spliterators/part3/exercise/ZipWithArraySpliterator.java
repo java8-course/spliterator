588f4d852c205d6742f0de221820d873d4666d8c
Aleksandr_Petukhov <aleksandr_petukhov@epam.com>
2017-04-13 18:45:17 +0300

    ZipWithArraySpliterator works

--- src/main/java/spliterators/part3/exercise/ZipWithArraySpliterator.java
+++ src/main/java/spliterators/part3/exercise/ZipWithArraySpliterator.java
@@ -1,7 +1,5 @@
 package spliterators.part3.exercise;
 
-import spliterators.part2.exercise.IndexedDoublePair;
-
 import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.function.Consumer;
@@ -11,7 +9,7 @@ public class ZipWithArraySpliterator<A, B> extends Spliterators.AbstractSplitera
 
     private final Spliterator<A> inner;
     private final B[] array;
-    private final int endIndex;
+    private final int endIndexExc;
     private int currentIndex;
 
     public ZipWithArraySpliterator(Spliterator<A> inner, B[] array) {
@@ -23,7 +21,7 @@ public class ZipWithArraySpliterator<A, B> extends Spliterators.AbstractSplitera
         this.inner = inner;
         this.array = array;
         currentIndex = startIndex;
-        this.endIndex = endIndex;
+        this.endIndexExc = endIndex;
     }
 
     @Override
@@ -33,25 +31,27 @@ public class ZipWithArraySpliterator<A, B> extends Spliterators.AbstractSplitera
 
     @Override
     public boolean tryAdvance(Consumer<? super Pair<A, B>> action) {
-        return currentIndex < endIndex && inner.tryAdvance(a -> action.accept(new Pair<A, B>(a, array[currentIndex++])));
+        return currentIndex < endIndexExc && inner.tryAdvance(a -> action.accept(new Pair<A, B>(a, array[currentIndex++])));
     }
 
     @Override
     public void forEachRemaining(Consumer<? super Pair<A, B>> action) {
-        while (currentIndex < endIndex) {
-            currentIndex++;
-            if (!inner.tryAdvance(a -> action.accept(new Pair<A, B>(a, array[currentIndex])))) return;
+        while (currentIndex < endIndexExc) {
+            if (!inner.tryAdvance(a -> action.accept(new Pair<A, B>(a, array[currentIndex++])))) return;
         }
     }
 
     @Override
     public Spliterator<Pair<A, B>> trySplit() {
+
+        if (array.length - currentIndex < 2) return null;
+
         if (inner.hasCharacteristics(SUBSIZED)) {
             Spliterator<A> splittedInner = inner.trySplit();
             currentIndex += splittedInner.estimateSize();
-
-            return new ZipWithArraySpliterator<>(splittedInner, array,
-                    (int)(currentIndex - splittedInner.estimateSize()), currentIndex);
+            int start = (int) (currentIndex - splittedInner.estimateSize());
+            int end = currentIndex < array.length ?  currentIndex: array.length;
+            return new ZipWithArraySpliterator<>(splittedInner, array, start, end);
         }
 
         return super.trySplit();
@@ -59,7 +59,7 @@ public class ZipWithArraySpliterator<A, B> extends Spliterators.AbstractSplitera
 
     @Override
     public long estimateSize() {
-        if (inner.hasCharacteristics(SIZED)) return Math.min(inner.estimateSize(), endIndex - currentIndex);
-        return endIndex - currentIndex;
+        if (inner.hasCharacteristics(SIZED)) return Math.min(inner.estimateSize(), endIndexExc - currentIndex+1);
+        return endIndexExc - currentIndex+1;
     }
 }
