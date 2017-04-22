4540a0b3a768b1e4f3410171f75f02c270c1b4db
Aleksandra Pankratova <aleksandra_pankratova@epam.com>
2017-04-22 23:36:55 +0300

    - part3 is done

--- src/main/java/spliterators/part3/exercise/ZipWithArraySpliterator.java
+++ src/main/java/spliterators/part3/exercise/ZipWithArraySpliterator.java
@@ -9,40 +9,53 @@ public class ZipWithArraySpliterator<A, B> extends Spliterators.AbstractSplitera
 
     private final Spliterator<A> inner;
     private final B[] array;
+    private int index;
 
     public ZipWithArraySpliterator(Spliterator<A> inner, B[] array) {
-        super(Long.MAX_VALUE, 0); // FIXME:
-        // TODO
-        throw new UnsupportedOperationException();
+        super(Math.min(inner.estimateSize(), array.length), inner.characteristics());
+        this.inner = inner;
+        this.array = array;
+        index = 0;
     }
 
     @Override
     public int characteristics() {
-        // TODO
-        throw new UnsupportedOperationException();
+        return inner.characteristics();
     }
 
     @Override
     public boolean tryAdvance(Consumer<? super Pair<A, B>> action) {
-        // TODO
-        throw new UnsupportedOperationException();
+        return index < array.length && inner.tryAdvance(a -> {
+            action.accept(new Pair<>(a, array[index]));
+            index++;
+        });
     }
 
     @Override
     public void forEachRemaining(Consumer<? super Pair<A, B>> action) {
-        // TODO
-        throw new UnsupportedOperationException();
+        inner.forEachRemaining(a -> {
+            action.accept(new Pair<>(a, array[index]));
+            index++;
+        });
     }
 
     @Override
     public Spliterator<Pair<A, B>> trySplit() {
-        // TODO
-        throw new UnsupportedOperationException();
+        if (!inner.hasCharacteristics(SUBSIZED)) return super.trySplit();
+        Spliterator<A> split = inner.trySplit();
+        if (split == null) return null;
+        final ZipWithArraySpliterator<A, B> spliterator = new ZipWithArraySpliterator<>(split, array);
+        spliterator.setIndex(index);
+        index = Math.min((int) (index + split.estimateSize()), array.length);
+        return spliterator;
     }
 
     @Override
     public long estimateSize() {
-        // TODO
-        throw new UnsupportedOperationException();
+        return Math.min(inner.estimateSize(), array.length - index);
+    }
+
+    private void setIndex(int index) {
+        this.index = index;
     }
 }
