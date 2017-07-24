a514e139291fd11c9488690c35500c1284f24082
Narek Karapetyan <narek.27.02.93@mail.ru>
2017-07-25 00:23:04 +0300

    Done with some qualitative tests

--- src/test/java/spliterators/part2/exercise/ZipWithIndexDoubleSpliteratorTest.java
+++ src/test/java/spliterators/part2/exercise/ZipWithIndexDoubleSpliteratorTest.java
@@ -3,8 +3,13 @@ package spliterators.part2.exercise;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Spliterator;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
+import static java.util.stream.Collectors.toList;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -12,6 +17,37 @@ import static org.junit.Assert.*;
  * 24.07.2017 by K.N.K
  */
 public class ZipWithIndexDoubleSpliteratorTest {
+
+    private double[] getRandomArray(int length) {
+        final double[] result = new double[length];
+
+        for (int i = 0; i < length; i++) {
+            result[i] = ThreadLocalRandom.current().nextDouble();
+        }
+
+        return result;
+    }
+
+    @Test
+    public void comparePaired() {
+        final double[] randomArray = getRandomArray(1_000);
+
+        final List<IndexedDoublePair> result1 = Stream.iterate(
+                new IndexedDoublePair(0, randomArray[0]),
+                p -> new IndexedDoublePair(p.getIndex() + 1, randomArray[p.getIndex() + 1]))
+                .limit(randomArray.length)
+                .collect(toList());
+
+        final Spliterator.OfDouble spliterator =
+                Arrays.stream(randomArray).spliterator();
+        final List<IndexedDoublePair> result2 =
+                StreamSupport.stream(new ZipWithIndexDoubleSpliterator(spliterator), true)
+                        .map(p -> new IndexedDoublePair(p.getIndex() + 1, p.getValue()))
+                        .map(p -> new IndexedDoublePair(p.getIndex() - 1, p.getValue()))
+                        .collect(toList());
+
+        assertEquals(result1, result2);
+    }
 
     @Test
     public void testZipWithIndexSplitAndTryAdvance() throws Exception {
