f8f5517c5eddf34ff035d7aecc6cd89abd0e2e18
Iartsev Kirill <whobscr@gmail.com>
2017-07-31 14:54:40 +0300

    part2 completed

--- src/main/java/spliterators/part2/exercise/ZipWithIndexDoubleSpliterator.java
+++ src/main/java/spliterators/part2/exercise/ZipWithIndexDoubleSpliterator.java
@@ -3,22 +3,23 @@ package spliterators.part2.exercise;
 import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Consumer;
 
 public class ZipWithIndexDoubleSpliterator extends Spliterators.AbstractSpliterator<IndexedDoublePair> {
 
 
     private final OfDouble inner;
-    private AtomicInteger currentIndex;
+    private AtomicLong currentIndex;
 
     public ZipWithIndexDoubleSpliterator(OfDouble inner) {
         this(0, inner);
     }
 
-    private ZipWithIndexDoubleSpliterator(int firstIndex, OfDouble inner) {
+    private ZipWithIndexDoubleSpliterator(long firstIndex, OfDouble inner) {
         super(inner.estimateSize(), inner.characteristics());
         if (! inner.hasCharacteristics(SUBSIZED)) throw new IllegalStateException("Zip got not subsized Spliterator");
-        currentIndex = new AtomicInteger(firstIndex);
+        currentIndex = new AtomicLong(firstIndex);
         this.inner = inner;
     }
 
@@ -34,7 +35,7 @@ public class ZipWithIndexDoubleSpliterator extends Spliterators.AbstractSplitera
         // TODO
         final boolean res =
                 inner.tryAdvance((Double v) ->
-                        action.accept(new IndexedDoublePair(currentIndex.get(), v)));
+                        action.accept(new IndexedDoublePair((int)currentIndex.get(), v)));
         if (res) currentIndex.incrementAndGet();
         return res;
     }
@@ -43,7 +44,7 @@ public class ZipWithIndexDoubleSpliterator extends Spliterators.AbstractSplitera
     public void forEachRemaining(Consumer<? super IndexedDoublePair> action) {
         // TODO
         inner.forEachRemaining((Double v) -> {
-            action.accept(new IndexedDoublePair(currentIndex.get(), v));
+            action.accept(new IndexedDoublePair((int)currentIndex.get(), v));
             currentIndex.incrementAndGet();
         });
     }
@@ -52,7 +53,13 @@ public class ZipWithIndexDoubleSpliterator extends Spliterators.AbstractSplitera
     public Spliterator<IndexedDoublePair> trySplit() {
         // TODO
          if (inner.hasCharacteristics(SUBSIZED)) {
-             return new ZipWithIndexDoubleSpliterator(inner.trySplit());
+             OfDouble newSplit = inner.trySplit();
+             if (newSplit == null)
+                 return null;
+             Spliterator<IndexedDoublePair> zipped =
+                     new ZipWithIndexDoubleSpliterator(currentIndex.get(), newSplit);
+             currentIndex.addAndGet(newSplit.estimateSize());
+             return zipped;
         } else return super.trySplit();
     }
 
